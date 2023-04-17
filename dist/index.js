0cecfba98a1f0ccd794ca60f1c38b0c2b923fb46
kkumar-gcc <krishkumar9352@gmail.com>
2023-04-17 16:38:03 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Pagination", {
+  enumerable: true,
+  get: function get() {
+    return _pagination.default;
+  }
+});
+var _pagination = _interopRequireDefault(require("./components/pagination"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
